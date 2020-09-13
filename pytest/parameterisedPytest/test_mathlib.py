517fae1a4971524fe0da213c686d515334d1e679
Danusha <danusha70754@gmail.com>
2020-09-13 17:17:13 +0530

    Adding a function with multiple param for pytest

--- pytest/parameterisedPytest/test_mathlib.py
+++ pytest/parameterisedPytest/test_mathlib.py
@@ -1,5 +1,7 @@
 import mathlib
 import pytest
+# import numpy
+
 @pytest.mark.parametrize("test_input, expected_output",
                          [
                              (5,25),
@@ -9,4 +11,17 @@ import pytest
                          ])
 def test_calc_square(test_input , expected_output):
     result = mathlib.square(test_input)
+    assert result == expected_output
+
+@pytest.mark.parametrize("test_input, expected_output",
+                         [
+                             ((5,5),25),
+
+                         ])
+def test_calMultiply(test_input , expected_output):
+    print(test_input)
+    result = 1
+    for x in test_input:
+        result *=x
+
     assert result == expected_output
